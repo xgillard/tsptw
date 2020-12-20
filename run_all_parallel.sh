11b8c414784f56ee628bc303b28282df092d3b22
Xavier Gillard <xavier.gillard@uclouvain.be>
2020-12-20 09:01:08 +0100

    produce results in a separate folder

--- run_all_parallel.sh
+++ run_all_parallel.sh
@@ -5,17 +5,18 @@
 duration=1800
 
 rm progress.txt
+mkdir -p results
 
-#bench=Langevin
-#width=10000
-#dest="${bench}_w${width}_t${duration}.txt"
-#echo "Starting $bench" >> progress.txt
-#./target/release/tsptw print-header > $dest
-#find ./tests/resources/$bench -type f | parallel -I% ./target/release/tsptw solve % -t 1 -w $width -d $duration >> $dest
-#
+bench=Langevin
+width=100
+dest="results/${bench}_w${width}_t${duration}.txt"
+echo "Starting $bench" >> progress.txt
+./target/release/tsptw print-header > $dest
+find ./tests/resources/$bench -type f | parallel -I% ./target/release/tsptw solve % -t 1 -w $width -d $duration >> $dest
+
 bench=SolomonPotvinBengio
 width=100
-dest="${bench}_w${width}_t${duration}.txt"
+dest="results/${bench}_w${width}_t${duration}.txt"
 echo "Starting $bench" >> progress.txt
 ./target/release/tsptw print-header > $dest
 find ./tests/resources/$bench -type f | parallel -I% ./target/release/tsptw solve % -t 1 -w $width -d $duration >> $dest
@@ -23,35 +24,35 @@ find ./tests/resources/$bench -type f | parallel -I% ./target/release/tsptw solv
 
 bench=Dumas
 width=100
-dest="${bench}_w${width}_t${duration}.txt"
+dest="results/${bench}_w${width}_t${duration}.txt"
 echo "Starting $bench" >> progress.txt
 ./target/release/tsptw print-header > $dest
 find ./tests/resources/$bench -type f | parallel -I% ./target/release/tsptw solve % -t 1 -w $width -d $duration >> $dest
 
 bench=GendreauDumasExtended
 width=100
-dest="${bench}_w${width}_t${duration}.txt"
+dest="results/${bench}_w${width}_t${duration}.txt"
 echo "Starting $bench" >> progress.txt
 ./target/release/tsptw print-header > $dest
 find ./tests/resources/$bench -type f | parallel -I% ./target/release/tsptw solve % -t 1 -w $width -d $duration >> $dest
 
 bench=OhlmannThomas
 width=100
-dest="${bench}_w${width}_t${duration}.txt"
+dest="results/${bench}_w${width}_t${duration}.txt"
 echo "Starting $bench" >> progress.txt
 ./target/release/tsptw print-header > $dest
 find ./tests/resources/$bench -type f | parallel -I% ./target/release/tsptw solve % -t 1 -w $width -d $duration >> $dest
 
 bench=SolomonPesant
 width=100
-dest="${bench}_w${width}_t${duration}.txt"
+dest="results/${bench}_w${width}_t${duration}.txt"
 echo "Starting $bench" >> progress.txt
 ./target/release/tsptw print-header > $dest
 find ./tests/resources/$bench -type f | parallel -I% ./target/release/tsptw solve % -t 1 -w $width -d $duration >> $dest
 
 bench=AFG
 width=100
-dest="${bench}_w${width}_t${duration}.txt"
+dest="results/${bench}_w${width}_t${duration}.txt"
 echo "Starting $bench" >> progress.txt
 ./target/release/tsptw print-header > $dest
 find ./tests/resources/$bench -type f | parallel -I% ./target/release/tsptw solve % -t 1 -w $width -d $duration >> $dest
